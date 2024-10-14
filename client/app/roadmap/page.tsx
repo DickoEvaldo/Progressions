66fce170a13edc2354d218b882e21eba55f7a40b
Dicko Evaldo <z5451386@ad.unsw.edu.au>
2024-10-14 21:12:42 +1100

    installed dependencies

---
+++ client/app/roadmap/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+
+const page = () => {
+  return (
+    <div>
+      <Button>aa</Button>
+    </div>
+  );
+};
+
+export default page;
